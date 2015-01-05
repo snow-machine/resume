36fe32c619ca7119b6c7bb316f863192a0a19459
snow-machine <119238122@qq.com>
2015-01-05 15:36:11 +0800

    create resume

---
+++ auto/face.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook "face"
+ (lambda ()
+    (LaTeX-add-environments
+     "shell")
+    (TeX-add-symbols
+     "HRule")))
+
---
+++ auto/resume.el
@@ -0,0 +1,20 @@
+(TeX-add-style-hook "resume"
+ (lambda ()
+    (TeX-run-style-hooks
+     "framed"
+     "setspace"
+     "hyperref"
+     "fancyhdr"
+     "wrapfig"
+     "graphicx"
+     "amssymb"
+     "amsmath"
+     "latexsym"
+     "latex2e"
+     "ctexart10"
+     "ctexart"
+     "UTF8"
+     "face"
+     "kno1"
+     "cou1")))
+
